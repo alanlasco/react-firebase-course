6622fa30e7d79a4875a313765058474df6951a52
alanlasco <61526254+alanlasco@users.noreply.github.com>
2023-11-11 20:03:02 -0300

    edit function

--- crud-react/src/components/NameList.tsx
+++ crud-react/src/components/NameList.tsx
@@ -10,6 +10,14 @@ export const NameList = () => {
 
     const [nombre, setNombre] = useState<string>('');
     const [lista, setLista] = useState<tnombre[]>([]);
+    const [edicion, setEdicion] = useState<boolean>(false);
+    const [id, setId] = useState<string>('');
+
+    const editar = (item:tnombre) =>{
+        setEdicion(true);
+        setNombre(item.n);
+        setId(item.id);
+    }
 
     const addNombre = (e: React.FormEvent<HTMLFormElement>) =>{
         e.preventDefault()
@@ -24,6 +32,14 @@ export const NameList = () => {
         const nArray = lista.filter( item => item.id !==id)
         setLista(nArray);
     }
+    const editNombre = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const newArray = lista.map(item => item.id === id ?{id:id, n:nombre} : item)
+        setLista(newArray);
+        setEdicion(false);
+        setNombre('');
+
+    }
 
   return (
     <>
@@ -36,9 +52,14 @@ export const NameList = () => {
                             <li key={item.id} className='list-group-item'>{item.n}
                             <button
                             onClick={()=> {deleteNombre(item.id)}} 
-                            className='btn btn-danger float-right ms-3'>
+                            className='btn btn-danger float-end ms-3'>
                             borrar
                             </button>
+                            <button
+                            onClick={()=> {editar(item)}} 
+                            className='btn btn-info float-end ms-3'>
+                            editar
+                            </button>                            
                             </li>
                             )
         
@@ -47,10 +68,11 @@ export const NameList = () => {
             </div>
             <div className="col">
                 <h2>Formulario</h2>
-            <form onSubmit={(e)=> addNombre(e)} className='form-group col' action="">
+                  <form onSubmit={edicion ? (e) =>{editNombre(e)} : (e) => addNombre(e)} className='form-group col' action="">
                 <input onChange={(e)=>{setNombre(e.target.value)}} className='form-control mb-3' type="text" name="" id="" placeholder='introduce el nombre' value={nombre} />
                 <div className="d-grid gap-2">
-                <input className='btn btn-info' type="submit" value="registrar nombre" />
+                <input className='btn btn-info' type="submit" 
+                value={edicion ? 'EDITAR NOMBRE' : 'Registrar Nombre'} />
                 </div>
             </form>            
             </div>
